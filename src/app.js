9e434012779f175470f0e03e99e6be3816450dc0
tanbiranjum <tanbiranjum@hotmail.com>
2022-09-02 23:44:52 +0600

    all api added

--- src/app.js
+++ src/app.js
@@ -3,7 +3,23 @@ const getCatagory = async () => {
     "https://openapi.programming-hero.com/api/news/categories"
   );
   const { data } = await res.json();
-  const categories = data.map((category) => category.category_name);
+  return data.news_category
 };
 
 getCatagory();
+
+const getNewsById = async(category_id)=> {
+    const res = await fetch(`https://openapi.programming-hero.com/api/news/category/${category_id}`)
+    const result = await res.json()
+    console.log(result)
+}
+
+const getNewsDetails = async (news_id)=> {
+    const res = await fetch(`https://openapi.programming-hero.com/api/news/${news_id}`)
+    const result = await res.json()
+    console.log(result)
+}
+
+getNewsById("01")
+
+
