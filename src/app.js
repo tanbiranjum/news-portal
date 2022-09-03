ec14fc36b42387850726bfd8cfb2f36997e92c9f
tanbiranjum <tanbiranjum@hotmail.com>
2022-09-03 20:32:41 +0600

    spinner added

--- src/app.js
+++ src/app.js
@@ -1,54 +1,59 @@
-let showSpinner = false
-let currentCategory = "01"
+let currentCategory = "01";
 
 const cardsElement = document.querySelector(".cards");
-const categoryElement = document.querySelector(".category-list")
-const modalElement = document.querySelector(".modal-dialog")
-
+const categoryElement = document.querySelector(".category-list");
+const modalElement = document.querySelector(".modal-dialog");
+const spinnerElement = document.querySelector(".spinner");
 
 const getCatagory = async () => {
   const res = await fetch(
     "https://openapi.programming-hero.com/api/news/categories"
   );
   const { data } = await res.json();
-  return data.news_category
+  return data.news_category;
 };
 
 getCatagory();
 
-const getNewsById = async(category_id)=> {
-    const res = await fetch(`https://openapi.programming-hero.com/api/news/category/${category_id}`)
-    const {data} = await res.json()
-    return data
-}
+const getNewsById = async (category_id) => {
+  showLoadingSpinner();
+  const res = await fetch(
+    `https://openapi.programming-hero.com/api/news/category/${category_id}`
+  );
+  const { data } = await res.json();
+  hideLoadingSpinner();
+  return data;
+};
 
-const getNewsDetails = async (news_id)=> {
-    const res = await fetch(`https://openapi.programming-hero.com/api/news/${news_id}`)
-    const result = await res.json()
-    console.log(result)
-    return result
-}
+const getNewsDetails = async (news_id) => {
+  const res = await fetch(
+    `https://openapi.programming-hero.com/api/news/${news_id}`
+  );
+  const result = await res.json();
+  console.log(result);
+  return result;
+};
 
-
-const renderCategory = async()=> {
-  const categories = await getCatagory()
-  categories.map(category=> {
+const renderCategory = async () => {
+  const categories = await getCatagory();
+  categories.map((category) => {
     categoryElement.innerHTML += `
     <li class="list-inline-item">
       <a href="#" class="text-decoration-none text-primary" onClick="renderCards('${category.category_id}')">${category.category_name}</a>
     </li>
-    `
-  })
-}
+    `;
+  });
+};
 
-renderCategory()
+renderCategory();
 
-const renderCards = async (categoryID = currentCategory)=> {
-  const cards = await getNewsById(categoryID)
-  cardsElement.innerHTML = ""
-  cards.map(card=> {
-    cardsElement.innerHTML = cardsElement.innerHTML +
-    `<div class="card mb-3 p-3">
+const renderCards = async (categoryID = currentCategory) => {
+  const cards = await getNewsById(categoryID);
+  cardsElement.innerHTML = "";
+  cards.map((card) => {
+    cardsElement.innerHTML =
+      cardsElement.innerHTML +
+      `<div class="card mb-3 p-3">
     <div class="row g-0">
       <div class="col-auto">
         <img
@@ -101,14 +106,14 @@ const renderCards = async (categoryID = currentCategory)=> {
         </div>
       </div>
     </div>
-  </div>`
-  })
-}
+  </div>`;
+  });
+};
 
-const renderModal = async (newsId)=> {
-  const {data} = await getNewsDetails(newsId)
-  const news = data[0]
-  modalElement.innerHTML = ''
+const renderModal = async (newsId) => {
+  const { data } = await getNewsDetails(newsId);
+  const news = data[0];
+  modalElement.innerHTML = "";
   modalElement.innerHTML = `
   <div class="modal-content">
   <div class="modal-header">
@@ -134,22 +139,22 @@ const renderModal = async (newsId)=> {
     <button type="button" class="btn btn-primary">
       Rating <span class="badge text-bg-secondary">${news.rating.number}</span>
     </button>
-    <p>Published: <span>${new Date(news.author.published_date).toDateString()}</span></p>
+    <p>Published: <span>${new Date(
+      news.author.published_date
+    ).toDateString()}</span></p>
   </div>
 </div>
 
-  `
-}
+  `;
+};
 
-const renderLoadingSpinner = ()=> {
-  return `
-  <div class="spinner-border" role="status">
-  <span class="visually-hidden">Loading...</span>
-  </div>
-  `
-}
+const showLoadingSpinner = () => {
+  spinnerElement.classList.remove("loading-spinner-active");
+};
 
-renderCards()
+const hideLoadingSpinner = () => {
+  spinnerElement.classList.add("loading-spinner-active");
+};
 
-// Render Function which get triggers by navbar category
-// Modal Get Trigger by view more with newsID
+renderCards();
+
