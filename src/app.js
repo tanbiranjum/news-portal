2690319989d5539983283dbea1c8918916ec833f
tanbiranjum <tanbiranjum@hotmail.com>
2022-09-04 00:04:57 +0600

    card two column added

--- src/app.js
+++ src/app.js
@@ -2,18 +2,18 @@ const cardsElement = document.querySelector(".cards");
 const categoryElement = document.querySelector(".category-list");
 const modalElement = document.querySelector(".modal-dialog");
 const spinnerElement = document.querySelector(".spinner");
-const contentInfoElement = document.querySelector(".content-info")
+const contentInfoElement = document.querySelector(".content-info");
 
 const getCatagory = async () => {
   try {
     const res = await fetch(
       "https://openapi.programming-hero.com/api/news/categories"
-    ).catch(()=> false);
-    if(res.status !== 200) alert("You are in offline!")
+    ).catch(() => false);
+    if (res.status !== 200) alert("You are in offline!");
     const { data } = await res.json();
     return data.news_category;
   } catch (error) {
-    return false
+    return false;
   }
 };
 
@@ -24,19 +24,19 @@ const getNewsById = async (category_id) => {
     showLoadingSpinner();
     const res = await fetch(
       `https://openapi.programming-hero.com/api/news/category/${category_id}`
-    ).catch(()=> false);
+    ).catch(() => false);
     const { data } = await res.json();
     hideLoadingSpinner();
-    return data; 
+    return data;
   } catch (error) {
-    return false
+    return false;
   }
 };
 
 const getNewsDetails = async (news_id) => {
   const res = await fetch(
     `https://openapi.programming-hero.com/api/news/${news_id}`
-  ).catch(()=> false);
+  ).catch(() => false);
   const result = await res.json();
   console.log(result);
   return result;
@@ -53,79 +53,93 @@ const renderCategory = async () => {
   });
 };
 
-renderCategory();
-
 const renderCards = async (category_id, category_name) => {
-  const categoryID = category_id || "01"
-  const cards = await getNewsById(categoryID);
-  const lists = document.querySelectorAll(".list-inline-item")
-  Array.from(lists).forEach(list=> list.classList.remove('list-inline-active'))
-  Array.from(lists).forEach(list=> {
-    if(list.dataset.index === category_id) {
-      list.classList.add('list-inline-active')
+  const categoryID = category_id || "01";
+  const unsortedCards = await getNewsById(categoryID);
+  const cards = unsortedCards.sort(
+    (a, b) => b.total_view * 1 - a.total_view * 1
+  );
+  const lists = document.querySelectorAll(".list-inline-item");
+  Array.from(lists).forEach((list) =>
+    list.classList.remove("list-inline-active")
+  );
+  console.log(lists);
+  Array.from(lists).forEach((list) => {
+    if (list.dataset.index === category_id) {
+      list.classList.add("list-inline-active");
     }
-  })
-  if(!cards) return "Something went wrong!"
-  if(!cards.length) return cardsElement.innerHTML = "Sorry! There are no News in this section"
-  contentInfoElement.innerHTML = `<h6>${cards.length} items found for category ${category_name || "Breaking News"}</h6>`
+  });
+  if (!cards) return "Something went wrong!";
+  if (!cards.length)
+    return (cardsElement.innerHTML =
+      "Sorry! There are no News in this section");
+  contentInfoElement.innerHTML = `<h6>${
+    cards.length
+  } items found for category ${category_name || "Breaking News"}</h6>`;
   cardsElement.innerHTML = "";
   cards.map((card) => {
     cardsElement.innerHTML =
       cardsElement.innerHTML +
-      `<div class="card mb-3 p-3">
+      `
+      <div class="col-md-6">
+      <div class="card p-3">
     <div class="row g-0">
-      <div class="col-auto">
+      <div class="col-md-5">
         <img
           src="${card.thumbnail_url}"
           class="img-fluid rounded-start"
           alt=""
         />
       </div>
-      <div class="col-md-8">
-        <div class="card-body">
-          <h5 class="card-title">
-            ${card.title}
+      <div class="col-md-7 align-self-center">
+        <div class="card-body pt-0">
+        <div>
+              <img src="./assets/bxs_star-half.png" alt=""/>
+              <img src="./assets/Vector.png" alt=""/>
+              <img src="./assets/Vector.png" alt=""/>
+              <img src="./assets/Vector.png" alt=""/>
+              <img src="./assets/Vector.png" alt=""/>
+            </div>
+        <p class="mb-0">${
+          new Date(card.author.published_date).toDateString() || "No data found"
+        }</p>
+          <h5 class="card-title h5">
+            ${card.title || "No data found"}
           </h5>
           <p class="card-text ellipsis">
-            ${card.details}
+            ${card.details || "No data found"}
           </p>
           <div
             class="d-flex justify-content-between align-items-center"
           >
-            <div class="d-flex">
+            <div class="d-flex align-items-center">
               <img
                 src="${card.author.img}"
                 alt=""
-                class="rounded-circle"
-                height="50"
-                width="50"
+                class="rounded-circle me-1"
+                height="40"
+                width="40"
               />
-              <div class="card-text">
-                <p>Jane Cooper</p>
-                <p>Jan 10, 2022</p>
-              </div>
+                <p>${card.author.name || "No data found"}</p>
             </div>
             <div class="d-inline">
               <img src="./assets/carbon_view.png" alt="eye icon" />
-              <p class="d-inline">${card.total_view}</p>
-            </div>
-            <div>
-              <img src="./assets/bxs_star-half.png" alt="" />
-              <img src="./assets/Vector.png" alt="" />
-              <img src="./assets/Vector.png" alt="" />
-              <img src="./assets/Vector.png" alt="" />
-              <img src="./assets/Vector.png" alt="" />
+              <p class="d-inline">${card.total_view || "No data found"}</p>
             </div>
             <div>
               <a href="#" data-bs-toggle="modal" data-bs-target="#exampleModal">
-                <img src="./assets/Group.png" alt="" onClick="renderModal('${card._id}')"/>
+                <img src="./assets/Group.png" alt="" onClick="renderModal('${
+                  card._id
+                }')"/>
               </a>
             </div>
           </div>
         </div>
       </div>
     </div>
-  </div>`;
+  </div>
+  </div>
+  `;
   });
 };
 
@@ -137,7 +151,7 @@ const renderModal = async (newsId) => {
   <div class="modal-content">
   <div class="modal-header">
     <h5 class="modal-title" id="exampleModalLabel">
-      ${news.title}
+      ${news.title || "No data found"}
     </h5>
     <button
       type="button"
@@ -151,16 +165,18 @@ const renderModal = async (newsId) => {
     alt=""
   />
   <div class="modal-body">
-  ${news.details}
+  ${news.details || "No data found"}
   </div>
   <div class="modal-footer justify-content-between">
-    <h6>Author: ${news.author.name}</h6>
+    <h6>Author: ${news.author.name || "No data found"}</h6>
     <button type="button" class="btn btn-primary">
-      Rating <span class="badge text-bg-secondary">${news.rating.number}</span>
+      Rating <span class="badge text-bg-secondary">${
+        news.rating.number || "No data found"
+      }</span>
     </button>
-    <p>Published: <span>${new Date(
-      news.author.published_date
-    ).toDateString()}</span></p>
+    <p>Published: <span>${
+      new Date(news.author.published_date).toDateString() || "No data found"
+    }</span></p>
   </div>
 </div>
 
@@ -175,5 +191,5 @@ const hideLoadingSpinner = () => {
   spinnerElement.classList.add("loading-spinner-active");
 };
 
+renderCategory();
 renderCards();
-
